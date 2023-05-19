3598a4c832ea0d626ac6d2ceaa810103818b15c8
Camille Scott <camille.scott.w@gmail.com>
2023-05-19 13:02:50 -0700

    Add errors.py

---
+++ cheeto/errors.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Camille Scott, 2023
+# (c) The Regents of the University of California, Davis, 2023
+# File   : errors.py
+# License: Modified BSD
+# Author : Camille Scott <cswel@ucdavis.edu>
+# Date   : 15.05.2023
+
+from enum import Enum, IntEnum
+
+
+class ExitCode(IntEnum):
+    VALIDATION_ERROR = 1
+    BAD_MERGE = 2
+    INVALID_SPONSOR = 3
+
