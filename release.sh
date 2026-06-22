7c7a74616dcecf06590eddbeaf107bd2062b6cbe
Camille Scott <cswel@ucdavis.edu>
2026-06-21 20:23:35 -0700

    Fix atomic push format

--- release.sh
+++ release.sh
@@ -28,4 +28,4 @@ print "cheeto: bumped ${old} -> ${new}; committing, tagging v${new}, and pushing
 git add pyproject.toml cheeto/__init__.py
 git commit -m v$new
 git tag v$new
-git push --atomic v$new
+git push --atomic origin $(git rev-parse --abbrev-ref HEAD) v$new
